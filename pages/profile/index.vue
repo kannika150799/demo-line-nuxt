db8490095390d612fe74746d7360a6f077b3717f
kannika150799 <kannika_1507@hotmail.com>
2021-02-11 16:27:50 +0700

    make it better

---
+++ pages/profile/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="~/assets/IMG.jpg" />
+    <p>userID: </p>
+    <p>displayName: </p>
+    <p>statusMessage: </p>
+
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+img {
+    width: 25%;
+}
+</style>
