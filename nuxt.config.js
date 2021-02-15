1de0b43112e528d434ff0ab2ece698c8ea24e8fa
kannika150799 <kannika_1507@hotmail.com>
2021-02-15 10:51:40 +0700

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'demo-line-nuxt',
+    title: 'profile',
     htmlAttrs: {
       lang: 'en'
     },
