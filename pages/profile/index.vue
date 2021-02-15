82ee5f6ad135fc54907cb700b1ce4f1d74b9095b
kannika150799 <kannika_1507@hotmail.com>
2021-02-15 11:37:18 +0700

    get profile

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <img src="~/assets/IMG.jpg" />
+    <img v-if="profileImage == ''" src="~/assets/IMG.jpg" alt="">
+    <img v-else :src="profileImage" alt="">
     <p>userID: </p>
     <p>displayName: </p>
     <p>statusMessage: </p>
@@ -9,6 +10,33 @@
 </template>
 
 <script>
+import liff from '@line/liff';
+
+export default { 
+  mounted(){
+    liff.init({
+      liffId: '1655660869-VoKZDYDO'
+    }).then(() => {
+      if(liff.isLoggedIn()){
+        liff.getProfile().then(profile => {                    
+          this.profileImage = profile.pictureUrl
+        })
+      }else{
+        liff.login();
+      }
+    })
+  },
+  data(){    
+    return { 
+      profileImage: '',     
+      form: {
+        firstname: this.$store.getters.getRegister.firstname,
+        lastname: this.$store.getters.getRegister.lastname,
+        gender: this.$store.getters.getRegister.gender       
+      },      
+    }
+  }
+}
 </script>
 
 <style scoped>
