d9482f3e600c5fe8079956f650f1314dc3369192
kannika150799 <kannika_1507@hotmail.com>
2021-02-18 21:42:13 +0700

    test2

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -9,7 +9,7 @@
     <div class="container-input">
       <template>
         <div class="box-input">
-          <p class="text-input name">Name7</p>
+          <p class="text-input name">Name8</p>
           <a-input placeholder="Name" />
         </div>
         <div class="box-input">
