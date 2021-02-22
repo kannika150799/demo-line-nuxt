10a03378aaa42c9f2ed1431edd95670b9e5e91f9
kannika150799 <kannika_1507@hotmail.com>
2021-02-22 15:31:53 +0700

    update code calendar

---
+++ pages/calendar/listCalendar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <p>กิจกรรม</p>
+    <div>
+      <p>กิจกรรม: (ชื่อกิจกรรม)</p>
+      <p>วันที่: (วันที่)</p>
+      <div>
+        <a-button class="edit-button" type="primary" @click="editList">
+          Edit
+        </a-button>
+        <a-button class="delete-button" type="primary" @click="deleteList">
+          Delete
+        </a-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {
+    editList() {
+        this.$router.push('/calendar/editCalendar')
+    },
+    deleteList() {}
+  },
+};
+</script>
+
+<style scoped>
+</style>
