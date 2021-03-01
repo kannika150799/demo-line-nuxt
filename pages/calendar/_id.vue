605f227d5a3b60f9ca9eef6d447d57811856aefb
kannika150799 <kannika_1507@hotmail.com>
2021-03-01 11:33:43 +0700

    put calendar

--- pages/calendar/_id.vue
+++ pages/calendar/_id.vue
@@ -5,7 +5,7 @@
       <template>
         <div class="box-input">
           <p class="text-input">ชื่อกิจกรรม</p>
-          <a-input class="input" v-model="calendar.activity" placeholder="ชื่อกิจกรรม"/>
+          <a-input class="input" placeholder="ชื่อกิจกรรม"/>
           <!-- <a-input class="input" >ชื่อกิจกรรม</a-input> -->
         </div>
       </template>
