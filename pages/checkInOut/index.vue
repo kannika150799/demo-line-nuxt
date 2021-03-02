d859b15b33de622ab323f20c833d4b858cb1e07d
kannika150799 <kannika_1507@hotmail.com>
2021-03-02 16:57:15 +0700

    update code

--- pages/checkInOut/index.vue
+++ pages/checkInOut/index.vue
@@ -54,12 +54,12 @@ export default {
     isDone(){
       this.$axios.get(`https://db-back.herokuapp.com/api/get/check/${this.inOut.userId}`).then((res) => {
         if(res.data != null){
-          console.log('res', res.data);
-          this.isActiveIn = false;
-          this.isActiveOut =  true;
-        }else {
+          // console.log('res', res.data);
           this.isActiveIn = true;
           this.isActiveOut =  false;
+        }else if (res.data == null) {
+          this.isActiveIn = false;
+          this.isActiveOut =  true;
         }
 
       });
